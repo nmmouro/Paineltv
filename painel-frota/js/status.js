3e78c85a9c45701846663d0d6b8d84c04303706a
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-15 20:26:42 -0300

    Update status.js

--- painel-frota/js/status.js
+++ painel-frota/js/status.js
@@ -1,1 +1,33 @@
+export const STATUS = {
 
+    atendido:{
+        icon:"✔",
+        class:"status-atendido"
+    },
+
+    concluido:{
+        icon:"✔",
+        class:"status-concluido"
+    },
+
+    andamento:{
+        icon:"⏳",
+        class:"status-andamento"
+    },
+
+    cancelado:{
+        icon:"✖",
+        class:"status-cancelado"
+    },
+
+    manutenção:{
+        icon:"🔧",
+        class:"status-manutencao"
+    },
+
+    viagem:{
+        icon:"✈️",
+        class:"status-viagem"
+    }
+
+};
