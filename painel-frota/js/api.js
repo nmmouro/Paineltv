f0c1a46372f78ae63afe84fcd34facc25f058f82
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-15 20:27:22 -0300

    Add fetchSheet function to retrieve Google Sheets data

--- painel-frota/js/api.js
+++ painel-frota/js/api.js
@@ -1,1 +1,17 @@
+import { SHEET_ID } from "./config.js";
+import { parseGviz } from "./helpers.js";
 
+export async function fetchSheet(sheet, query){
+
+    const url =
+    `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq`+
+    `?sheet=${encodeURIComponent(sheet)}`+
+    `&tq=${encodeURIComponent(query)}`+
+    `&tqx=out:json`;
+
+    const res = await fetch(url);
+
+    return parseGviz(
+        await res.text()
+    );
+}
