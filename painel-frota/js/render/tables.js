3c9f4c4fa60719d2209727f356f1969843e3bc58
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-15 20:28:40 -0300

    Update tables.js

--- painel-frota/js/render/tables.js
+++ painel-frota/js/render/tables.js
@@ -1,1 +1,59 @@
+import {
+    getStatusClass,
+    getStatusIcon
+}
+from "../status.js";
 
+export function renderTable(
+    headers,
+    rows
+){
+
+return `
+<table>
+
+<thead>
+<tr>
+${headers.map(h => `<th>${h}</th>`).join("")}
+</tr>
+</thead>
+
+<tbody>
+
+${rows.map(row => {
+
+const values =
+(row.c || []).map(
+c => c?.f ?? c?.v ?? ""
+);
+
+const status =
+values.at(-1);
+
+return `
+<tr class="${getStatusClass(status)}">
+
+${values.map((v,i)=>`
+
+<td class="${
+i === values.length - 1
+? 'status-icon'
+: ''
+}">
+${i === values.length - 1
+? getStatusIcon(status)
+: v}
+</td>
+
+`).join("")}
+
+</tr>
+`;
+
+}).join("")}
+
+</tbody>
+
+</table>
+`;
+}
