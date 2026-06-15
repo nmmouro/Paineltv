20098f12c8e7bd4cbc9fd4ee599f185b1d97e9f2
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-15 20:27:00 -0300

    Update helpers.js

--- painel-frota/js/helpers.js
+++ painel-frota/js/helpers.js
@@ -1,1 +1,26 @@
+export function parseGviz(txt){
 
+    return JSON.parse(
+        txt.substring(
+            txt.indexOf("{"),
+            txt.lastIndexOf("}") + 1
+        )
+    );
+}
+
+export function getCell(c){
+
+    return c?.f ?? c?.v ?? "";
+}
+
+export function formatarData(valor){
+
+    if(!valor) return "";
+
+    if(valor instanceof Date){
+
+        return valor.toLocaleDateString("pt-BR");
+    }
+
+    return String(valor);
+}
